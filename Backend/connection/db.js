e1c82182efc9d4f72f8e1a9684ff4eac52d4624f
MSaifKhan01 <msaifkhan5038@gmail.com>
2023-10-03 01:12:23 +0530

    MongoDB atlas added

--- Backend/connection/db.js
+++ Backend/connection/db.js
@@ -1,5 +1,9 @@
 const mongoose=require("mongoose")
 
-const connection=mongoose.connect("mongodb://127.0.0.1:27017/ShortUrl")
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const connection=mongoose.connect(process.env.mongourl)
 
 module.exports=connection
